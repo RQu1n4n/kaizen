d6c9b3dc913c90f92b818f268a1c45da977eccbc
Alexdc12345 <ajdc456@gmail.com>
2024-03-31 20:47:21 +0800

    Login and Signup Function

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
-    
-class KaizenUser(models.Model):
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class KaizenUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class KaizenUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField('User Email')
-    department = models.CharField(max_length=50)
-    password = models.CharField(max_length=20)
+    email = models.EmailField('User Email', unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(Group, related_name='kaizen_users')
+    user_permissions = models.ManyToManyField(Permission, related_name='kaizen_users')
+
+    objects = KaizenUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return self.first_name + '' + self.last_name
+        return self.first_name + ' ' + self.last_name
--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('articles', views.all_articles, name="article_list"),
-    path('login', views.user_login, name="login"),
-    path('sign-up', views.user_registration, name="register"),
+    path('login', views.login, name="login"),
+    path('sign-up', views.sign_up, name="sign-up"),
+    path('about', views.about, name="about"),
+    path('register/', views.register, name="reg"),
+    path('login/', views.user_login, name="log")
 ]
